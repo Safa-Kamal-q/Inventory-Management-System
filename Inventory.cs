ba619f393e61b4f42d7c7ad4881262cca62eb0cf
unknown <Safa.Qasrawi.K@gmail.com>
2024-03-15 16:10:26 +0200

    Initialize project, adding addItem method

---
+++ Inventory.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inventory_Management_System
+{
+    internal class Inventory
+    {
+        private List<Product> products;
+
+        public Inventory()
+        {
+            products = new List<Product>();
+        }
+
+        public void addingItem(Product product)
+        {
+            products.Add(product);
+            Console.WriteLine("Item added successfully");
+        }
+    }
+}
---
+++ Product.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inventory_Management_System
+{
+    internal class Product
+    {
+        private String name { get; set; }
+        private decimal price { get; set; }
+        private int quantity { get; set; }
+
+        public Product(String name, decimal price, int quantity) 
+        {
+            this.name = name;
+            this.price = price;
+            this.quantity = quantity;
+        }
+
+        public String display()
+        {
+            return $"Name: {name}, price: {price}, quantity: {quantity}";
+        }
+    }
+}
