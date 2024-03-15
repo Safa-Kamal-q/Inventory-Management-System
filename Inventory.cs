0405866d176768fd07112ce293d5c246e3cfda4c
unknown <Safa.Qasrawi.K@gmail.com>
2024-03-15 17:04:15 +0200

    adding DisplayAllProducts method

--- Inventory.cs
+++ Inventory.cs
@@ -16,10 +16,26 @@ namespace Inventory_Management_System
             products = new List<Product>();
         }
 
-        public void addingItem(Product product)
+        public void AddingItem(Product product)
         {
             products.Add(product);
             Console.WriteLine("Item added successfully");
+        }
+
+        public void DisplayingAllProducts()
+        {
+            if (products.Count == 0)
+            {
+                Console.WriteLine("No products to display");
+                return;
+            }
+
+            Console.WriteLine("All Products\n========================================");
+            foreach (Product product in products)
+            {
+                product.display();
+            }
+            Console.WriteLine("========================================");
         }
     }
 }
